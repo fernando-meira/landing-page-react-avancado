d9b1bc8bd47346952403773cc219b803ec74aa02
fernando-meira <thomwork@outlook.com>
2021-09-05 22:17:15 -0300

    feat: 🎸 Changed GET_LANDING_PAGE query

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,38 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
+  fragment imageData on UploadFile {
+    url
+    alternativeText
+  }
+
+  fragment button on ComponentPageButton {
+    url
+    label
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      ...imageData
+    }
+  }
+
+  fragment landingPage on LandingPage {
+    header {
+      title
+      description
+      image {
+        ...imageData
+      }
+      button {
+        ...button
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
-      logo {
-        url
-        alternativeText
-      }
+      ...logo
+
+      ...landingPage
     }
   }
 `;
