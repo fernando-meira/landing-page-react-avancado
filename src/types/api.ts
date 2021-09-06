8087df56dc0cc32bfaf2727ae94f70aa96ee408e
fernando-meira <thomwork@outlook.com>
2021-09-05 22:16:06 -0300

    feat: 🎸 Added Header types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,21 @@
-export type LogoProps = {
+export type ImageProps = {
   url: string;
   alternativeText: string;
 };
 
+type ButtonProps = {
+  url: string;
+  label: string;
+};
+
+export type HeaderProps = {
+  title: string;
+  image: ImageProps;
+  description: string;
+  button: ButtonProps;
+};
+
 export type LandingPageProps = {
-  logo: LogoProps;
+  logo: ImageProps;
+  header: HeaderProps;
 };
