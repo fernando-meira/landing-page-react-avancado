95d6b93ca98d6282c9cfe2c97401adbce128f5d5
fernando-meira <thomwork@outlook.com>
2021-09-13 00:47:44 -0300

    feat: 🎸 Integration of the Section Modules

--- src/components/SectionModules/index.tsx
+++ src/components/SectionModules/index.tsx
@@ -3,22 +3,26 @@ import React from 'react';
 import Heading from 'components/Heading';
 import Container from 'components/Container';
 import CardModule from 'components/CardModule';
+import { SectionModulesProps } from 'types/api';
 
-import content from './content';
 import * as S from './styles';
 
-const SectionModules = () => (
-  <Container>
-    <Heading reverseColor>Módulos deste curso</Heading>
+const SectionModules = ({ title, modules }: SectionModulesProps) => {
+  console.log(modules);
 
-    <S.Content>
-      {content.map(({ title, subtitle, description }, index) => (
-        <CardModule key={index} title={title} subTitle={subtitle}>
-          <div dangerouslySetInnerHTML={{ __html: description }} />
-        </CardModule>
-      ))}
-    </S.Content>
-  </Container>
-);
+  return (
+    <Container>
+      <Heading reverseColor>{title}</Heading>
+
+      <S.Content>
+        {modules.map(({ title, subtitle, description }, index) => (
+          <CardModule key={index} title={title} subTitle={subtitle}>
+            <div dangerouslySetInnerHTML={{ __html: description }} />
+          </CardModule>
+        ))}
+      </S.Content>
+    </Container>
+  );
+};
 
 export default SectionModules;
